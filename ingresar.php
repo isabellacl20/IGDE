adcc80d716ff3ae3da01fd9a80fcdb9e5c180613
Isabella <isabellacordobalopez02@hotmail.com>
2020-09-21 14:18:18 -0500

    Boton ingresar

---
+++ ingresar.php
@@ -0,0 +1,16 @@
+<form action="ingresar.php" method="POST">
+            <h1>INGRESAR</h1>
+            <div class="datosing">
+                <div id="labels">
+                    <label for="name">Usuario</label>
+                    <label for="contraseña">Contraseña</label>
+            </div>
+
+            <div id="inputs">
+                    <input type="text" name="nombreusuario" id="name" >
+                    <input type="password" name="contraseña">
+            </div>
+            
+     <input name="Ingresar" type="submit" value="Ingresar">
+
+    </form>
--- interaccion.php
+++ interaccion.php
@@ -100,6 +100,22 @@
      }
 
      ?>
+     <form action="interaccion.php" method="POST">
+            <h1>INGRESAR</h1>
+            <div class="datosing">
+                <div id="labels">
+                    <label for="name">Usuario</label>
+                    <label for="contraseña">Contraseña</label>
+            </div>
+
+            <div id="inputs">
+                    <input type="text" name="nombreusuario" id="name" >
+                    <input type="password" name="contraseña">
+            </div>
+            
+     <input name="Ingresar" type="submit" value="Ingresar">
+
+    </form>
      
      
      <script src="IGDE.js"></script> 
